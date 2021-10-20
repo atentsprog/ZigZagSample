8ef056dbadabd2bdfb1a65f42eabc59322bbbb4e
hktrollgames <hktrollgames@gmail.com>
2021-10-20 20:33:21 +0900

    큐브 랜덤 생성

---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform cubeParent;
+    public Cube baseItem;
+    public int makeCount = 20;
+
+    private void Awake()
+    {
+        GenerateCubes();
+    }
+
+    // 랜덤하게 큐브 생성 시키자.
+    [ContextMenu("큐브 생성")]
+    private void GenerateCubes()
+    {
+        // 기존 블럭 부스자.
+        DestroyExistCube();
+
+        Vector3 previousPos = Vector3.zero;
+        for (int i = 0; i < makeCount; i++)
+        {
+            var neweCube = Instantiate(baseItem, cubeParent);
+            neweCube.transform.localRotation = Quaternion.identity;
+
+            // previousPos의 x나 z를 1 증가하자.
+            if (Random.Range(0, 2) == 0)
+                previousPos += new Vector3(1, 0, 0);
+            else
+                previousPos += new Vector3(0, 0, 1);
+
+            neweCube.transform.localPosition = previousPos;
+            previousPos = neweCube.transform.localPosition;
+        }
+    }
+
+    private void DestroyExistCube()
+    {
+        var allCube = cubeParent.GetComponentsInChildren<Cube>();
+        foreach (var item in allCube)
+            DestroyImmediate(item.gameObject);
+    }
+}
---
+++ Assets/MoveOnlyZ.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnlyZ : MonoBehaviour
+{
+    public float x;
+    public float y;
+    void Start()
+    {
+        x = transform.position.x;
+        y = transform.position.y;
+    }
+
+    void LateUpdate()
+    {
+        var pos = transform.position;
+        pos.x = x;
+        pos.y = y;
+        transform.position = pos;
+    }
+}
